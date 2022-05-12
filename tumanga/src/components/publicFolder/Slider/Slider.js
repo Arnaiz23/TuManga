e07a1d21a679dd8c24e55b7eb0061756c4740a11
Adrián Arnaiz Fernández <aaf110501@gmail.com>
2022-05-12 17:41:52 +0200

    update: v1.1.46 	* Change the image in slider

--- tumanga/src/components/publicFolder/Slider/Slider.js
+++ tumanga/src/components/publicFolder/Slider/Slider.js
@@ -2,16 +2,17 @@ import React from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { api_URL } from "services/config";
 
 export default function Slider() {
     return (
         <div className="slider">
-            <button className="iconSlider iconSliderLeft" role="button"><i><FontAwesomeIcon icon={faAngleLeft} /></i></button>
+            <button className="iconSlider iconSliderLeft"><i><FontAwesomeIcon icon={faAngleLeft} /></i></button>
 
             {/* Complete the slider */}
-            <img src="https://wowslider.com/sliders/demo-18/data1/images/shanghai.jpg" alt="imagenes slider mangas" />
-            
-            <button className="iconSlider iconSliderRight" role="button"><i><FontAwesomeIcon icon={faAngleRight} /></i></button>
+            <img src={`${api_URL}/image/Slider1.png`} alt="imagenes slider mangas" />
+
+            <button className="iconSlider iconSliderRight"><i><FontAwesomeIcon icon={faAngleRight} /></i></button>
         </div>
     )
 }
