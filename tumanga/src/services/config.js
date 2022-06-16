66c1b8fc5057473bb203bf3dca7fefa307d91d6d
Arnaiz23 <aaf110501@gmail.com>
2022-06-16 13:37:11 +0200

    update: v2.0.20 	* Update the url of the backend

--- tumanga/src/services/config.js
+++ tumanga/src/services/config.js
@@ -1,5 +1,5 @@
 // export const api_URL = "http://localhost:3900/api/v1"
-export const api_URL = "https://tu-manga.herokuapp.com//api/v1"
+export const api_URL = "https://tu-manga.herokuapp.com/api/v1"
 
 export function getToken() {
     let token
