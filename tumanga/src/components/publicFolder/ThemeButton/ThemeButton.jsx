7b10198b4da68fa5eea1e431c1d0677c028c23b0
Arnaiz23 <aaf110501@gmail.com>
2023-11-02 22:17:27 +0100

    feat ✨: Add the initial theme state

--- tumanga/src/components/publicFolder/ThemeButton/ThemeButton.jsx
+++ tumanga/src/components/publicFolder/ThemeButton/ThemeButton.jsx
@@ -1,19 +1,37 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const ThemeButton = () => {
-  const [theme, setTheme] = useState("light")
+  const [theme, setTheme] = useState(() => {
+    if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+      return localStorage.getItem("theme")
+    }
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark"
+    }
+    return "light"
+  })
 
   const changeTheme = () => {
     if (theme === "dark") {
-      document.documentElement.classList.remove("dark")
       setTheme("light")
     } else {
       setTheme("dark")
-      document.documentElement.classList.add("dark")
     }
   }
+
+  useEffect(() => {
+    const root = document.documentElement
+
+    if (theme === "light") {
+      root.classList.remove("dark")
+    } else {
+      root.classList.add("dark")
+    }
+
+    localStorage.setItem("theme", theme)
+  })
 
   return (
     <button className="themeButton" onClick={changeTheme}>
